346f3031d1b8fb84b20a346f8fbd417a554aa7bb
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-02-24 20:57:33 -0500

    changed Rating.rating to Rating.value

--- db/migrate/20120225015453_create_ratings.rb
+++ db/migrate/20120225015453_create_ratings.rb
@@ -3,7 +3,7 @@ class CreateRatings < ActiveRecord::Migration
     create_table :ratings do |t|
       t.integer :post_id
       t.integer :user_id
-      t.integer :rating
+      t.integer :value
 
       t.timestamps
     end
