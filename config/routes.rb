aea6216e4eff8a578a4cfbd112e470f80a2f6fef
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-02-24 20:55:15 -0500

    created scaffolds for User, Post and Rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ShibShibBlastic::Application.routes.draw do
+  resources :ratings
+
+  resources :posts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20120225015453_create_ratings.rb
@@ -0,0 +1,11 @@
+class CreateRatings < ActiveRecord::Migration
+  def change
+    create_table :ratings do |t|
+      t.integer :post_id
+      t.integer :user_id
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
