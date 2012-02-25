35888ae39e993c3ed212690d295d1bffd59e0b20
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-02-24 20:46:40 -0500

    changed gemfile to use pg as production database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShibShibBlastic::Application.config.secret_token = '2f4a89081d1eda30320763cfd99a3979073148252381aca1ae7f0066999b2167aba64304ef7fc589c5ecabb9873d78ac65f522ff532de9eea2565c706490662e'
