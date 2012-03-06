b2d69067faf33e07051badee320ecee5ab661229
Farhan Hurmoodi <terfender@gmail.com>
2012-03-06 12:37:28 +0400

    user's forms, notice, alert

--- app/assets/javascripts/shibshib-v1.js
+++ app/assets/javascripts/shibshib-v1.js
@@ -1,29 +1,7 @@
-function getY()
-{
-	if(window.pageYOffset > 103){
-		document.getElementById("h3").style.position = "fixed";
-		document.getElementById("h3").style.top = "83px";
-		document.getElementById("postaPic").style.marginTop = "95px";
-	}
-	else {
-		document.getElementById("h3").style.position = "static";
-		document.getElementById("postaPic").style.marginTop = "50px";
-	}
-		
-	setTimeout(function(){getY()}, 50);
-	
-}
-window.onload = function () { 
-	getY();
-}
-
-
-$(document).ready(function(){
-	
-	$("#back-top").hide();
+$("#back-top").hide();
 	$(function () {
 		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
+			if ($(this).scrollTop() > 200) {
 				$('#back-top').fadeIn();
 			} else {
 				$('#back-top').fadeOut();
@@ -44,55 +22,46 @@ $(document).ready(function(){
 				});
 			return false;
 		});
-	});
+	});	
 
-});
-
-
-/////////////////////
-
-
-
-$(document).ready(function(){
-
-///////////////////////////////////////////////////////////////
-	$('div #m').each(function(index)
-	{
-		
-	    $(this).wrap("<div id=\"section"+index+"Wrapper\"><div id=\"section"+index+"\"></div></div>");
-		
-		//alert($("#section"+index+"Wrapper").parent().height());
-		//index = index + 1;
-		//alert(index);
-		//alert(window.pageYOffset);
-		///alert(window.pageYOffset)
-	});
+window.onload = function () {
 	
-	//$("#panel").append("<div id=\"section"+$("rightPosts").size()+"Wrapper\"></div>");
-	//$("#panel").before("<div id=\"section0Wrapper\"></div>");
+	getY();
 	
-	$(window).scroll(function(){
+};
 
-		$("div #m").each(function(index)
-		{
-			
-			
-			checkElem(index);
 
-		});
-
-	});
-
-///////////////////////////////////////////////////////////////
-	
-function checkElem(id)
+function getY()
 {
+	if(window.pageYOffset > 103){
+		document.getElementById("social").style.position = "fixed";
+		document.getElementById("social").style.top = "83px";
+		document.getElementById("postaPic").style.marginTop = "95px";
+	}
+	else {
+		document.getElementById("social").style.position = "static";
+		document.getElementById("postaPic").style.marginTop = "50px";
+	}
+			
+	setTimeout(function(){getY()}, 50);
+		
+}
+$(document).ready(function(){
+	
+
+	
+	$('div #m').each(function(index){ $(this).wrap("<div id=\"section"+index+"Wrapper\"><div id=\"section"+index+"\"></div></div>");});
+	
+	$(window).scroll(function(){$("div #m").each(function(index){checkElem(index);});});
+
+///////////////////////////////////////////////////////////////
+	
+	function checkElem(id)
+	{
 		var roof = window.pageYOffset + 75;
 		var picH = $('#section'+(id)+'Wrapper').siblings().height();
 		var panelH = $("#section"+(id)+" > #m").height();
 		
-		
-		//alert($('#section'+(id)+'Wrapper').siblings().height());
 	
 		var tresh = (picH - panelH) + top - 85;
 		
@@ -102,55 +71,42 @@ function checkElem(id)
 		
 		var nextTop = $('#section'+(id+1)+'Wrapper').position().top;
 		var backward = tresh - roof;
-		
-		/*else
-		{
-			var tresh = (postH - panelH) + top - 85;
-			var nextUp = $('#section'+(id+1)+'Wrapper').position().top - 290;
-			var backward = tresh - roof;
-			
-			
-			/*var cur = $('#section'+(id)+'Wrapper').position().top-y;
-			var prev = $('#section'+(id-1)+'Wrapper').position().top-y;
-			delta = $("#section"+(id-1)+"Wrapper").parent().height() - $('#section'+(id-1)+'Wrapper').position().top;*/
 
 		
 		var el = $('#section'+(id));
 
-		//if (offset < cur && offset > prev-60)
+
 		if ((roof > top && buttom > roof) || (roof > backward && roof < top) && nextTop < roof)
 			swim(el);
 		else if (roof < nextTop && top > roof)
 			original(el);
 		else if ((top < roof))
 			stick(el, (panelH+65), picH);
-
-}
+	}
 
 ///////////////////////////////////////////////////////////////
 	
-function swim(element)
-{
-	//element.removeClass("nail");
-	element.css('margin-top', '0%');
-	element.addClass("swim");
-	//alert("swiming");
-}
+	function swim(element)
+	{
+		element.css('margin-top', '0%');
+		element.addClass("swim");
+		//alert("swiming");
+	}
 ///////////////////////////////////////////////////////////////
-function original(element)
-{
-	element.css('margin-top', '0%');
-	element.removeClass("swim");
-	//alert("original");
-    //element.css('margin-top', '40%');
-
-}
+	function original(element)
+	{
+		element.css('margin-top', '0%');
+		element.removeClass("swim");
+		//alert("original");
+	
+	}
 ///////////////////////////////////////////////////////////////
-function stick(element, panelH, picH)
-{
-	element.removeClass("swim");
-    element.css('margin-top', (picH-panelH)+"px");
-	//alert("sticking "+(picH-panelH)+"px");
-}
+	function stick(element, panelH, picH)
+	{
+		element.removeClass("swim");
+		element.css('margin-top', (picH-panelH)+"px");
+	}
 
 });
+
+
