c2f7ff921d1f00e4e7cbec0dd151175ad06d5ee7
Farhan Hurmoodi <terfender@gmail.com>
2012-03-08 07:26:27 +0400

    cleaning layout v1

--- app/assets/javascripts/shibshib-v1.js
+++ app/assets/javascripts/shibshib-v1.js
@@ -89,21 +89,26 @@ $(document).ready(function(){
 	function swim(element)
 	{
 		element.css('margin-top', '0%');
-		element.addClass("swim");
+		element.css('position', 'fixed');
+		element.css('right', '29.6%');
+		element.css('top', '8%');
+		//element.addClass("swim");
 		//alert("swiming");
 	}
 ///////////////////////////////////////////////////////////////
 	function original(element)
 	{
 		element.css('margin-top', '0%');
-		element.removeClass("swim");
+		element.css('position', 'static');
+		//element.removeClass("swim");
 		//alert("original");
 	
 	}
 ///////////////////////////////////////////////////////////////
 	function stick(element, panelH, picH)
 	{
-		element.removeClass("swim");
+		element.css('margin-top', '0%');
+		element.css('position', 'static');
 		element.css('margin-top', (picH-panelH)+"px");
 	}
 
