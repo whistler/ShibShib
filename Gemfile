96c042eee8e675a23ca7fc7420b61c519938d371
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-03-08 23:47:36 -0500

    added facebook comments

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ end
 
 group :development, :test do
   gem 'sqlite3'
+  gem 'pry'
 end
 
 group :production do
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,11 @@ class User < ActiveRecord::Base
   
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token.extra.raw_info
+    binding.pry
     if user = User.where(:email => data.email).first
       user
     else # Create a user with a stub password. 
-      User.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+      User.create!(:name => data.name, :email => data.email, :password => Devise.friendly_token[0,20]) 
     end
   end
 end
