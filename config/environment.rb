5dd19c324fa5961ad15310b8eb7d523e7f81bb30
Farhan Hurmoodi <terfender@gmail.com>
2012-04-09 07:35:16 +0400

    test i18n fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ShibShibBlastic::Application.initialize!
 
-#fix rails-i18n translation missing errors in production
-I18n.load_path = Dir.glob("#{RAILS_ROOT}/locales/**/*.{rb,yml}")
-I18n.default_locale = 'en'
-I18n.reload!
-
