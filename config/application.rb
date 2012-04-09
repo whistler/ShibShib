77f88a632905f5c4f7849b5af1a4c5e8a73e1261
Farhan Hurmoodi <terfender@gmail.com>
2012-04-09 07:54:57 +0400

    test i18n fix

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,6 @@ module ShibShibBlastic
     config.before_configuration do
       I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
       I18n.locale = 'en'
-      I18n.reload!
     end
 
 end
