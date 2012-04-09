6c368b69342dc71ad28f146684e3066926b3a729
Farhan Hurmoodi <terfender@gmail.com>
2012-04-09 07:43:49 +0400

    test i18n fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ShibShibBlastic::Application.initialize!
 
-
-config.before_configuration do
-      I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-      I18n.locale = 'en'
-      I18n.reload!
-    end
-
