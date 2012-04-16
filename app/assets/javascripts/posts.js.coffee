88b760b684c2cf6cc922235bf73e2773f25953f0
Farhan Hurmoodi <terfender@gmail.com>
2012-04-16 04:25:59 +0400

    add loading.gif when fetching new posts + added back to top link

--- app/assets/javascripts/posts.js.coffee
+++ app/assets/javascripts/posts.js.coffee
@@ -1,13 +1,15 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
 jQuery ->
     $(window).scroll ->
       url = $('.pagination .next_page').attr('href')
+      img = $("<img />").attr('src', '/assets/loading.gif')
       #userPage = $('.userPage').attr('type')
+      trigger = false
       if url && $(window).scrollTop() > $(document).height() - $(window).height() - 550
-        img = $("<img />").attr('src', 'http://giantleague.com/load.gif')
+        $('.pagination').text("")
         $('.pagination').append(img);
-        $('.pagination').remove(img);
         $.getScript(url)
     $(window).scroll()
--- app/assets/javascripts/shibshib-v1.js
+++ app/assets/javascripts/shibshib-v1.js
@@ -1,50 +1,25 @@
-// from http://codesnippets.joyent.com/posts/show/835
-/*
-Position.GetWindowSize = function(w) {
-    var width, height;
-        w = w ? w : window;
-        this.width = w.innerWidth || (w.document.documentElement.clientWidth || w.document.body.clientWidth);
-        this.height = w.innerHeight || (w.document.documentElement.clientHeight || w.document.body.clientHeight);
+$(document).ready(function(){
 
-        return this;
-} */
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
 
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
 
-function loadRemainingItems(){
-  // compute amount of page below the current scroll position
-  var remaining = ($('wrapper').viewportOffset()[1] + $('wrapper').getHeight()) 
-                      - Position.GetWindowSize().height;
-  //compute height of bottom element
-  var last = $$(".contact").last().getHeight();
-
-  if(remaining < last*2 && !$('complete')){
-    if(Ajax.activeRequestCount == 0){
-      var url = "/contacts";
-      var last = $$(".contact").last().className.match(/[0-9]+/)[0];
-      new Ajax.Request(url, {
-        method: 'get',
-        parameters: 'last=' + last,
-        onLoading: function(){
-          $('loading').show();
-        },
-        onSuccess: function(xhr){
-          $('loading').hide();
-          $('loading').insert({before : xhr.responseText})
-        }
-      });
-    }
-  }
-}
-
-$(document).ready(function() {
-  $('pagination').hide();
 });
-
-// find to events that could fire loading items at the bottom
-/*Event.observe(window, 'scroll', function(e){
-  loadRemainingItems();
-});
-
-Event.observe(window, 'resize', function(e){
-  loadRemainingItems();
-});*/
