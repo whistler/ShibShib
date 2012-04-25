15ae8c94a07773f6df6e46eac06e686292b4a9a6
Farhan Hurmoodi <terfender@gmail.com>
2012-04-26 00:00:42 +0400

    fix user model on admin console + is_banned users can't post + is_admin users can edit posts from the site directly

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -11,4 +11,16 @@ ActiveAdmin.register User do
     column :last_sign_in_ip
     default_actions
   end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :email
+      f.input :is_banned
+      f.input :is_admin
+      f.input :oauth_provider
+      f.input :oauth_uid
+    end
+      f.buttons
+  end
 end
