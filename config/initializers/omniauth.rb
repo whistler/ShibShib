6c94fe354f4cb666fb8616e838f82903eaf32977
Farhan Hurmoodi <terfender@gmail.com>
2012-04-26 17:28:55 +0400

    add application keys to omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "254428867974812", "e6c5507c3f88d2341ef0c6103736e617", :display => 'popup'
+  provider :twitter, "eWjPI8AouBG2gsj94j2UNg", "vegxmi6w52olZ2188GRhWUYu7CoyNLHQ3KsRszh5A8", :display => 'popup'
 end
