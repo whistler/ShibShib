278b28c9b93c7a836f0019a9a5cdd9a6cce8cbec
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-04-28 22:52:06 -0400

    fixed oauth uids issues

--- db/migrate/20120428190604_add_auth_tokens_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthTokensToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :facebook_oauth_ui, :string
-
-    add_column :users, :twitter_oauth_ui, :string
-  end
-end
