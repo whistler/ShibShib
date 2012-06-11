b1bf30766009f9d3ed9527be7ef7246720b149ea
Terfender <terfender@gmail.com>
2012-06-12 02:24:14 +0400

    comment outh id drop command for testing

--- db/migrate/20120611220056_remove_outh_udi.rb
+++ db/migrate/20120611220056_remove_outh_udi.rb
@@ -1,7 +1,7 @@
 class RemoveOuthUdi < ActiveRecord::Migration
   def up
-    remove_column :users, :facebook_oauth_uid
-    remove_column :users, :twitter_oauth_uid
+    #remove_column :users, :facebook_oauth_uid
+    #remove_column :users, :twitter_oauth_uid
   end
 
   def down
