9601c6ba07762f7013bbc846586251b33934ff04
Terfender <terfender@gmail.com>
2015-03-10 13:27:23 -0400

    Update aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS.config({
-  :access_key_id     => 'AKIAIISMCRUVUPIWIV2A',
-  :secret_access_key => '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'
+  :access_key_id     => '',
+  :secret_access_key => ''
 })
